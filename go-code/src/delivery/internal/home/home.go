5a8cacb6da232b3d559cd9883985d03c0463d517
Jose Caceres <j6scaes@gmail.com>
2024-05-17 08:38:57 -0500

    feat: home returns data from ferretdb
    HomeService.Home returns data from db.

--- go-code/src/delivery/internal/home/home.go
+++ go-code/src/delivery/internal/home/home.go
@@ -7,6 +7,8 @@ import (
 	"github.com/joseluis8906/yummies/go-code/src/delivery/pkg/pb"
 	"github.com/joseluis8906/yummies/go-code/src/pkg/grpc"
 	"github.com/nats-io/nats.go"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.opentelemetry.io/otel"
 	"go.uber.org/fx"
 )
@@ -14,24 +16,72 @@ import (
 const authEmail string = "x-auth-email"
 
 type (
-	Service struct {
-		pb.UnimplementedHomeServiceServer
-		Nats *nats.Conn
-		Log  *log.Logger
-	}
-
 	Deps struct {
 		fx.In
 		Nats *nats.Conn
 		Log  *log.Logger
+		DB   *mongo.Client
+	}
+
+	Service struct {
+		pb.UnimplementedHomeServiceServer
+		Nats *nats.Conn
+		Log  *log.Logger
+		DB   *mongo.Database
+	}
+
+	Category struct {
+		Name string `bson:"name"`
+	}
+
+	TodaysSpecialOffer struct {
+		Name     string  `bson:"name"`
+		Price    Money   `bson:"price"`
+		Discount float32 `bson:"discount"`
+	}
+
+	PopularNow struct {
+		Name       string `bson:"name"`
+		Price      Money  `bson:"price"`
+		IsFavorite bool   `bson:"is_favorite"`
+	}
+
+	Money struct {
+		Amount   uint64 `bson:"amount"`
+		Decimals uint8  `bson:"decimals"`
+		Currency string `bson:"currency"`
 	}
 )
+
+func (m Money) PB() *pb.HomeMoney {
+	return &pb.HomeMoney{
+		Amount:   m.Amount,
+		Decimals: uint32(m.Decimals),
+		Currency: m.Currency,
+	}
+}
+
+func (tso TodaysSpecialOffer) PB() *pb.HomeTodaysSpecialOffer {
+	return &pb.HomeTodaysSpecialOffer{
+		Name:     tso.Name,
+		Price:    tso.Price.PB(),
+		Discount: tso.Discount,
+	}
+}
+
+func (pn PopularNow) PB() *pb.HomePopularNow {
+	return &pb.HomePopularNow{
+		Name:       pn.Name,
+		Price:      pn.Price.PB(),
+		IsFavorite: pn.IsFavorite,
+	}
+}
 
 func New(deps Deps) *Service {
 	s := &Service{
 		Nats: deps.Nats,
 		Log:  deps.Log,
-		// Stores: deps.Stores,
+		DB:   deps.DB.Database("delivery"),
 	}
 
 	return s
@@ -47,49 +97,49 @@ func (s *Service) Home(ctx context.Context, req *pb.HomeRequest) (*pb.HomeRespon
 		return nil, err
 	}
 
+	cur, err := s.DB.Collection("home.categories").Find(ctx, bson.D{})
+	if err != nil {
+		return nil, err
+	}
+
+	var dbCategories []Category
+	err = cur.All(ctx, &dbCategories)
+	if err != nil {
+		return nil, err
+	}
+
+	var dbTso TodaysSpecialOffer
+	err = s.DB.Collection("home.todaysspecialoffer").FindOne(ctx, bson.D{}).Decode(&dbTso)
+	if err != nil {
+		return nil, err
+	}
+
+	cur, err = s.DB.Collection("home.popularnow").Find(ctx, bson.D{})
+	if err != nil {
+		return nil, err
+	}
+
+	var dbPn []PopularNow
+	err = cur.All(ctx, &dbPn)
+	if err != nil {
+		return nil, err
+	}
+
+	categories := make([]string, len(dbCategories))
+	for i, v := range dbCategories {
+		categories[i] = v.Name
+	}
+
+	todaysSpecialOffer := dbTso.PB()
+	popularNow := make([]*pb.HomePopularNow, len(dbPn))
+	for i, v := range dbPn {
+		popularNow[i] = v.PB()
+	}
+
 	res := &pb.HomeResponse{
-		Categories: []string{"Meals", "Slides", "Snacks", "Drinks", "Healthy", "Fast Food"},
-		TodaysSpecialOffer: &pb.HomeTodaysSpecialOffer{
-			Name: "Yummies Special Burger",
-			Price: &pb.HomeMoney{
-				Amount:   275,
-				Currency: "USD",
-				Decimals: 2,
-			},
-			Discount: 10.0,
-			PriceDiscounted: &pb.HomeMoney{
-				Amount:   250,
-				Currency: "USD",
-				Decimals: 2,
-			},
-		},
-		PopularNow: []*pb.HomePopularNow{
-			{
-				Name: "Beef Salad",
-				Price: &pb.HomeMoney{
-					Amount:   425,
-					Decimals: 2,
-					Currency: "USD",
-				},
-				IsFavorite: true,
-			},
-			{
-				Name: "Spicy Noodless",
-				Price: &pb.HomeMoney{
-					Amount:   570,
-					Decimals: 2,
-					Currency: "USD",
-				},
-			},
-			{
-				Name: "Lasagna",
-				Price: &pb.HomeMoney{
-					Amount:   630,
-					Decimals: 2,
-					Currency: "USD",
-				},
-			},
-		},
+		Categories:         categories,
+		TodaysSpecialOffer: todaysSpecialOffer,
+		PopularNow:         popularNow,
 	}
 
 	return res, nil
